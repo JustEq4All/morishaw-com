d79b30507ddcfd9f3e49c1893fa5fb1d3fff556a
Gilberto Morishaw <gilberto@morishaw.com>
2026-09-01 22:44:31 +0200

    Sync Tina generated client after schema JSON-LD edit

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ cacheDir: '/root/.openclaw/workspace/website/astro-site/tina/__generated__/.cache/1787660204617', url: 'https://content.tinajs.io/2.4/content/e76c03ef-4caf-445c-a0a4-b4aab6854e5a/github/master', token: '418628364d0f10d6d1a20c9c417c83d6ffb88c4f', queries,  });
+export const client = createClient({ cacheDir: '/root/.openclaw/workspace/website/astro-site/tina/__generated__/.cache/1788295445984', url: 'https://content.tinajs.io/2.4/content/e76c03ef-4caf-445c-a0a4-b4aab6854e5a/github/master', token: '418628364d0f10d6d1a20c9c417c83d6ffb88c4f', queries,  });
 export default client;
   
